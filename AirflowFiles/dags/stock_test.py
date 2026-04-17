22cdb2b062675a0f85aa6a107415127687383c89
Axe-Blomq <blomqaxe@arcada.fi>
2026-04-17 10:24:21 +0300

    removing unnecesay files.

--- AirflowFiles/dags/stock_test.py
+++
@@ -1,9 +0,0 @@
-from airflow import DAG
-from airflow.operators.bash import BashOperator
-from datetime import datetime
-
-fetch_data = BashOperator(
-    task_id="fetch_data",
-    bash_command="python /tests/predict_test.py",
-)
-#chech ghg
